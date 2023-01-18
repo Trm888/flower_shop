d91b4a4c37eb48e2c4a6bca09ed420f40e4f796a
paulego <egopaul@inbox.ru>
2023-01-18 15:54:44 +0500

    Brought the models to the admin panel.

--- bot/admin.py
+++ bot/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User, Flower, Courier, Florist, Order
+
+
+@admin.register(User)
+class AdminUser(admin.ModelAdmin):
+    fields = ('full_name', 'chat_id', 'phone_number', 'balance', 'access', 'admin')
+
+
+@admin.register(Flower)
+class AdminFlower(admin.ModelAdmin):
+    fields = ('title', 'description', 'type', 'image', 'florist', 'price')
+
+
+@admin.register(Order)
+class AdminOrder(admin.ModelAdmin):
+    fields = ('user', 'flower', 'courier', 'address', 'delivery_date', 'count')
+
+
+@admin.register(Courier)
+class AdminCourier(admin.ModelAdmin):
+    fields = ('full_name', 'chat_id')
+
+
+@admin.register(Florist)
+class AdminFlorist(admin.ModelAdmin):
+    fields = ('full_name', 'chat_id')
