d42dd947fed1d6c5b5e5cd8b4f03c399208894fa
Nikita Sazonov <proger@MacBook-Pro-Nikita.local>
2023-01-18 21:45:24 +0300

    seeder func v0.0.1

---
+++ bot/management/commands/db_seeder.py
@@ -0,0 +1,29 @@
+from django.core.management.base import BaseCommand
+from bot.models import Florist, Flower
+import random
+import glob
+
+class Command(BaseCommand):    
+    def handle(self, *args, **options):
+        db_seeder_florist()
+        db_seeder_flower()
+
+def db_seeder_florist():
+    florist = Florist.objects.create(
+        full_name = 'Алла Пугачева',
+        chat_id = '1234493213'
+    )
+
+def db_seeder_flower():
+    images = glob.glob("./media/*")
+    flower_prices = [1000, 1250, 1500, 2000, 3500, 5500, 7500, 11000]
+    flower_types = ['Розы', 'Гвоздики', 'Хризантема', 'Пионы', 'Альстромерии', 'Астра', 'Азалия', 'Лютик']
+    for image_number, image_path in enumerate(images):
+        flower = Flower.objects.create(
+            title = f'Букет #{image_number}',
+            description = f'Описание букета #{image_number}',
+            type = random.choice(flower_types),
+            image = image_path,
+            florist_id = 1,
+            price = random.choice(flower_prices)
+        )
